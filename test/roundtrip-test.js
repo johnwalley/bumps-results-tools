b5178c4316b7385c9559c4f8c57ba4a8eafd43c5
johnwalley <john@walley.org.uk>
2018-08-10 20:49:14 +0100

    Add ad_format roundtrip test

--- test/roundtrip-test.js
+++ test/roundtrip-test.js
@@ -15,4 +15,17 @@ describe('round-trip', function() {
       });
     });
   });
+
+  describe('write_ad(read_ad())', function() {
+    var files = fs.readdirSync('./results/ad_format/');
+
+    files.forEach(function(file) {
+      it('correctly round-trips ' + file, function() {
+        const contents = fs.readFileSync('./results/ad_format/' + file, 'utf8');
+
+        var actual = utils.write_ad(utils.read_ad(contents));
+        assert.equal(actual, contents);
+      });
+    });
+  });
 });
