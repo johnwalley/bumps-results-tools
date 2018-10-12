c3fff1927074a6df450773734761da0b86e88bee
johnwalley <john@walley.org.uk>
2018-10-12 10:05:24 +0100

    Wrap conversion in try catch

--- scripts/convert_tg_to_ad.js
+++ scripts/convert_tg_to_ad.js
@@ -24,24 +24,29 @@ fs.readdir('./results/tg_format/', function(err, files) {
   files.forEach(function(file) {
     const contents = fs.readFileSync('./results/tg_format/' + file, 'utf8');
     console.log('Reading ' + file);
-    const event = utils.read_tg(contents);
-    numFiles++;
 
-    const set = setMap[/^[a-z]+/g.exec(file)[0]];
-    const year = /[0-9]+/g.exec(file)[0];
-    const gender = genderMap[file.match(/[a-z]+/g)[1]];
+    try {
+      const event = utils.read_tg(contents);
+      numFiles++;
 
-    const newFile = set + year + gender + '.txt';
+      const set = setMap[/^[a-z]+/g.exec(file)[0]];
+      const year = /[0-9]+/g.exec(file)[0];
+      const gender = genderMap[file.match(/[a-z]+/g)[1]];
 
-    fs.writeFile(
-      './results/ad_format/' + newFile,
-      utils.write_ad(utils.abbreviate(event)),
-      function() {
-        console.log(
-          'Successfully converted ' + file + ' to produce ' + newFile
-        );
-      }
-    );
+      const newFile = set + year + gender + '.txt';
+
+      fs.writeFile(
+        './results/ad_format/' + newFile,
+        utils.write_ad(utils.abbreviate(event)),
+        function() {
+          console.log(
+            'Successfully converted ' + file + ' to produce ' + newFile
+          );
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
   });
 
   console.log(`Found ${numFiles} files`);
