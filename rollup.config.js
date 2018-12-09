a206519336008c5b9284e343ece36801ff8cf5c3
johnwalley <john@walley.org.uk>
2018-12-09 15:31:05 +0000

    Add chart generator

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+export default [
+	// browser-friendly UMD build
+	{
+		input: 'src/util.js',
+		output: {
+			name: 'bumpsResultsTools',
+			file: pkg.browser,
+			format: 'umd'
+		},
+		plugins: [
+			resolve(),
+			commonjs()
+		]
+	},
+	{
+		input: 'src/util.js',
+		external: ['lodash', 'd3'],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+];
