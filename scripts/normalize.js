c9534377e6105562c4ade0b39c9b49246530d0d1
johnwalley <john@walley.org.uk>
2023-07-23 22:54:40 +0100

    Use abbreviations when normalizing

--- scripts/normalize.js
+++ scripts/normalize.js
@@ -6,7 +6,7 @@ var files = fs.readdirSync("./results/tg_format/");
 files.forEach(function (file) {
   const contents = fs.readFileSync("./results/tg_format/" + file, "utf8");
 
-  var actual = utils.write_tg(utils.read_tg(contents));
+  var actual = utils.write_tg(utils.abbreviate(utils.read_tg(contents)));
 
   fs.writeFile("./results/tg_format/" + file, actual, function () {
     console.log("Successfully normalized " + file);
