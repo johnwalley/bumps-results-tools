aa6af6c5bae4e0b75e027c487e165934fa82ba3b
johnwalley <john@walley.org.uk>
2023-07-23 23:19:57 +0100

    feat: statistics

---
+++ scripts/generate_statistics.js
@@ -0,0 +1,49 @@
+var utils = require("../src/util");
+var statistics = require("../src/stats");
+var fs = require("fs");
+
+const events = [];
+
+fs.readdir("./results/ad_format/", function (err, files) {
+  if (err) throw err;
+
+  let numFiles = 0;
+
+  files.forEach(function (file) {
+    console.log(`Reading ${file}`);
+    const contents = fs.readFileSync("./results/ad_format/" + file, "utf8");
+    const event = utils.read_ad(contents);
+    numFiles++;
+    events.push(event);
+  });
+
+  let stats = {};
+
+  for (const small of ["Eights", "Lents", "Mays", "Torpids", "Town"]) {
+    stats[small.toLocaleLowerCase()] = {};
+
+    for (const gender of ["Men", "Women"]) {
+      stats[small.toLocaleLowerCase()][gender.toLocaleLowerCase()] = {};
+
+      const e = events.filter(
+        (event) => event.gender === gender && event.small === small,
+      );
+
+      const ncrews = statistics.ncrews(e);
+      const nhead = statistics.nhead(e);
+      const movdo = statistics.movdo(e);
+      const movup = statistics.movup(e);
+
+      stats[small.toLocaleLowerCase()][gender.toLocaleLowerCase()] = {
+        movdo,
+        movup,
+        ncrews,
+        nhead,
+      };
+    }
+  }
+
+  fs.writeFile("./stats.json", JSON.stringify(stats), function () {
+    console.log("Successfully wrote file to ./stats.json");
+  });
+});
---
+++ src/stats.js
@@ -0,0 +1,231 @@
+const dataForge = require("data-forge");
+
+function movdo(events) {
+  let res = {};
+
+  for (const event of events) {
+    const numCrews = event.divisions.reduce(
+      (sum, div) => (sum += div.length),
+      0,
+    );
+
+    event.divisions.forEach((div, index) => {
+      let currentMove = [];
+      let currentPos = [];
+
+      for (let day = 0; day < event.days + 1; day++) {
+        currentMove.push([]);
+        currentPos.push([]);
+        for (let crew = 0; crew < numCrews; crew++) {
+          currentPos[day].push(crew);
+        }
+      }
+
+      div.forEach((crew, crewIndex) => {
+        let position = crewIndex;
+        let currentDivision = index;
+        let change = 0;
+
+        for (let day = 0; day < event.days; day++) {
+          change += event.move[day][currentDivision][position];
+          position -= event.move[day][currentDivision][position];
+
+          if (position < 0) {
+            currentDivision -= 1;
+            position += event.divisions[currentDivision].length;
+          }
+
+          if (position >= event.divisions[currentDivision].length) {
+            position -= event.divisions[currentDivision].length;
+            currentDivision += 1;
+          }
+        }
+
+        if (res[crew]) {
+          if (change <= res[crew].fall) {
+            res[crew] = { crew: crew, fall: change, year: event.year };
+          }
+        } else {
+          res[crew] = { crew: crew, fall: change, year: event.year };
+        }
+      });
+    });
+  }
+
+  const df = new dataForge.DataFrame(Object.values(res));
+
+  return df.orderBy((column) => column.fall).toArray();
+}
+
+function movup(events) {
+  let res = {};
+
+  for (const event of events) {
+    const numCrews = event.divisions.reduce(
+      (sum, div) => (sum += div.length),
+      0,
+    );
+
+    event.divisions.forEach((div, index) => {
+      let currentMove = [];
+      let currentPos = [];
+
+      for (let day = 0; day < event.days + 1; day++) {
+        currentMove.push([]);
+        currentPos.push([]);
+        for (let crew = 0; crew < numCrews; crew++) {
+          currentPos[day].push(crew);
+        }
+      }
+
+      div.forEach((crew, crewIndex) => {
+        let position = crewIndex;
+        let currentDivision = index;
+        let change = 0;
+
+        for (let day = 0; day < event.days; day++) {
+          change += event.move[day][currentDivision][position];
+          position -= event.move[day][currentDivision][position];
+
+          if (position < 0) {
+            currentDivision -= 1;
+            position += event.divisions[currentDivision].length;
+          }
+
+          if (position >= event.divisions[currentDivision].length) {
+            position -= event.divisions[currentDivision].length;
+            currentDivision += 1;
+          }
+        }
+
+        if (res[crew]) {
+          if (change >= res[crew].rise) {
+            res[crew] = { crew: crew, rise: change, year: event.year };
+          }
+        } else {
+          res[crew] = { crew: crew, rise: change, year: event.year };
+        }
+      });
+    });
+  }
+
+  const df = new dataForge.DataFrame(Object.values(res));
+
+  return df.orderBy((column) => -column.rise).toArray();
+}
+
+function movup(events) {
+  let res = {};
+
+  for (const event of events) {
+    const numCrews = event.divisions.reduce(
+      (sum, div) => (sum += div.length),
+      0,
+    );
+
+    event.divisions.forEach((div, index) => {
+      let currentMove = [];
+      let currentPos = [];
+
+      for (let day = 0; day < event.days + 1; day++) {
+        currentMove.push([]);
+        currentPos.push([]);
+        for (let crew = 0; crew < numCrews; crew++) {
+          currentPos[day].push(crew);
+        }
+      }
+
+      div.forEach((crew, crewIndex) => {
+        let position = crewIndex;
+        let currentDivision = index;
+        let change = 0;
+
+        for (let day = 0; day < event.days; day++) {
+          change += event.move[day][currentDivision][position];
+          position -= event.move[day][currentDivision][position];
+
+          if (position < 0) {
+            currentDivision -= 1;
+            position += event.divisions[currentDivision].length;
+          }
+
+          if (position >= event.divisions[currentDivision].length) {
+            position -= event.divisions[currentDivision].length;
+            currentDivision += 1;
+          }
+        }
+
+        if (res[crew]) {
+          if (change >= res[crew].rise) {
+            res[crew] = { crew: crew, rise: change, year: event.year };
+          }
+        } else {
+          res[crew] = { crew: crew, rise: change, year: event.year };
+        }
+      });
+    });
+  }
+
+  const df = new dataForge.DataFrame(Object.values(res));
+
+  return df.orderBy((column) => -column.rise).toArray();
+}
+
+function ncrews(events) {
+  const rows = events.map((event) => {
+    const clubs = event.divisions
+      .flatMap((division) => division)
+      .map((crew) => ({
+        year: event.year,
+        club: crew.replace(/[0-9]+$/, "").trim(),
+      }));
+
+    const df = new dataForge.DataFrame(clubs);
+
+    const summarized = df
+      .groupBy((row) => row.club)
+      .select((group) => ({
+        year: group.last().year,
+        count: group.deflate((row) => row.club).count(), // Sum sales per client.
+        club: group.last().club,
+      }))
+      .inflate() // Series -> dataframe.
+      .orderBy((column) => -column.year)
+      .orderBy((column) => -column.count)
+      .toArray(); // Convert to regular JS array.
+
+    return summarized;
+  });
+
+  return rows.flatMap((row) => row);
+}
+
+function nhead(events) {
+  const rows = events.map((event) => ({
+    year: event.year,
+    head: event.finish[0][0],
+  }));
+
+  const df = new dataForge.DataFrame(rows);
+
+  const summarized = df
+    .groupBy((row) => row.head)
+    .select((group) => ({
+      year: group.last().year,
+      count: group.deflate((row) => row.head).count(), // Sum sales per client.
+      crew: group.last().head,
+    }))
+    .inflate() // Series -> dataframe.
+    .orderBy((column) => -column.year)
+    .orderBy((column) => -column.count)
+    .toArray(); // Convert to regular JS array.
+
+  return summarized;
+}
+
+module.exports = {
+  movdo: movdo,
+  movup: movup,
+  ncrews: ncrews,
+  nhead: nhead,
+};
