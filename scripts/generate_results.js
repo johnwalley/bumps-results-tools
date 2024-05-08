4c19670c8f5d4692ebd4a315f746ee9a14029a32
johnwalley <john@walley.org.uk>
2024-05-08 17:28:49 +0100

    Refactor generate_results.js to remove unused code block

--- scripts/generate_results.js
+++ scripts/generate_results.js
@@ -56,18 +56,6 @@ fs.readdir("./results/ad_format/", function (err, files) {
           )}`
         );
       });
-
-      
-      exec("cat *.js bad_file | wc -l", (err, stdout, stderr) => {
-        if (err) {
-          // node couldn't execute the command
-          return;
-        }
-
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
     }
   }
 });
