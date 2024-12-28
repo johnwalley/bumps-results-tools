a5c596aece2a9e93c97b6c3ee77a5749c8b5ff2b
johnwalley <john@walley.org.uk>
2024-12-28 23:11:33 +0000

    Remove unused files

--- _src/types/set.ts
+++
@@ -1,9 +0,0 @@
-export const Set = {
-  EIGHTS: "Summer Eights",
-  TORPIDS: "Torpids",
-  LENTS: "Lent Bumps",
-  MAYS: "May Bumps",
-  TOWN: "Town Bumps",
-} as const;
-
-export type Set = (typeof Set)[keyof typeof Set];
